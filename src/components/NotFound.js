32998b7d419de44d971ac39376468bcb193fc470
Carina Steinberger <carina.steinberger@gmail.com>
2021-04-20 17:05:26 +0200

    SignUp

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,14 @@
-import React from "react";
+const NotFound = () => {
+  return (
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <img
+          src='https://cdn.dribbble.com/users/159078/screenshots/2839066/page404.png?compress=1&resize=400x300'
+          alt='not found'
+        />
+      </div>
+    </div>
+  );
+};
 
-export default function NotFound() {
-  return <div>NotFounddsdsada</div>;
-}
+export default NotFound;
---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated ? <Route {...rest} component={Component} /> : <Redirect to='/sign-in' />;
+};
+
+export default ProtectedRoute;
