d3ff0250a373f41321fa46e5ffba7e5c952ed90e
Carina Steinberger <carina.steinberger@gmail.com>
2021-04-22 12:52:50 +0200

    Landingpage experience

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from "./App";
+import AuthState from './context/AuthContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <AuthState>
+      <App />
+      </AuthState>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
