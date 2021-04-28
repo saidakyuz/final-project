82b9b865b34ebf6b5c9ee714c35aa9c171dee024
Carina Steinberger <carina.steinberger@gmail.com>
2021-04-28 18:30:32 +0200

    create sidebar to map

--- src/components/EntranceGate.js
+++ src/components/EntranceGate.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Map from "./Map";
-//import SideNavbar from "./SideNavbar";
+import Sidebar from "./Sidebar";
 
 const EntranceGate = () => {
   return (
-  <div>
-  <Map />
-  {/* <SideNavbar /> */}
+  <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
+  <Sidebar width={300} height={'100vh'}>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+  </Sidebar>
+  <Map/>
   </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,44 @@
-const Sidebar = () => {
-    return
-}
+import {useState, useEffect, Fragment} from "react";
+import "../App.css";
 
+const Sidebar = ({ width, height, children }) => {
+  const [xPosition, setX] = useState(width);
 
-export default Sidebar;
+  const toggleMenu = () => {
+    if (xPosition > 0) {
+      setX(0);
+    } else {
+      setX(width);
+    }
+  };
+
+  useEffect(() => {
+    setX(0);
+  }, []);
+
+  return (
+    <Fragment>
+      <div
+        className="side-bar"
+        style={{
+          transformOrigin: 'right',
+          transform: `translatex(${xPosition}px)`,
+          width: width,
+          minHeight: height
+
+        }}
+      >
+        <button
+          onClick={() => toggleMenu()}
+          className="toggle-menu"
+          style={{
+            transform: `translate(-10px, 20vh)`
+          }}
+        ></button>
+        <div className="content">{children}</div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Sidebar
