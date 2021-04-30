f344b2f61ee1af4e01da9b9126cb9e7c7d729802
Carina Steinberger <carina.steinberger@gmail.com>
2021-04-30 17:06:40 +0200

    add icons and searchbar now visible again

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 // import * as React from 'react';
 import { useState, useRef, useCallback } from "react";
 import MapGL from "react-map-gl";
