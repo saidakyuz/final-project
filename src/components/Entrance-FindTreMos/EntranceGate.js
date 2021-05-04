45a151d1d5149da1c8dd330dd899aae0aee714aa
saidakyuz <saidakyuz@gmail.com>
2021-05-04 12:33:09 +0200

    corrections on sidebar

--- src/components/Entrance-FindTreMos/EntranceGate.js
+++ src/components/Entrance-FindTreMos/EntranceGate.js
@@ -9,17 +9,33 @@ import { Link } from "react-router-dom";
 
 const EntranceGate = () => {
   return (
-  <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-  <Sidebar width={150} height={'100vh'}>
-    <div>
-      <Link to="/createatremo"><img src={CreateBlackTremo} alt="Create Tremo" width="150px" /></Link>
-      <Link to="/createsingletremoreo"><img src={CreateSingleTremoRoute} alt="Create Single Tremo Route" width="150px" /></Link>
-      <Link to="/"><img src={CreateMultipleTremosRoute} alt="Create Multiple Tremos Route" width="150px" /></Link>
-      <Link to="/"><img src={TreasureChest} alt="TreasureChest" width="150px" /></Link>
+    <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+      <Sidebar width={180} height={"100vh"}>
+        <div className="container">
+          <Link to="/createatremo">
+            <img src={CreateBlackTremo} alt="Create Tremo" width="150px" />
+          </Link>
+          <Link to="/createsingletremoreo">
+            <img
+              src={CreateSingleTremoRoute}
+              alt="Create Single Tremo Route"
+              width="150px"
+            />
+          </Link>
+          <Link to="/">
+            <img
+              src={CreateMultipleTremosRoute}
+              alt="Create Multiple Tremos Route"
+              width="150px"
+            />
+          </Link>
+          <Link to="/">
+            <img src={TreasureChest} alt="TreasureChest" width="150px" />
+          </Link>
+        </div>
+      </Sidebar>
+      <FindTremoGeolocation />
     </div>
-  </Sidebar>
-  <FindTremoGeolocation />
-  </div>
   );
 };
 
