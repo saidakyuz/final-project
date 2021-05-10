30bda36a4a6bbcdb2e7ce4108dceef02e51985c4
Carina Steinberger <carina.steinberger@gmail.com>
2021-05-10 23:15:55 +0200

    meine Nerven

--- src/components/CreateATreMo/CreateATreMo.js
+++ src/components/CreateATreMo/CreateATreMo.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import Sidebar from '../Entrance-FindTreMos/Sidebar';
-import CreateTremoGeolocation from './CreateTremoGeolocation';
-import { Link } from 'react-router-dom';
-import Eye_illuminati_pyramid_triangle from '../../assets/Eye_illuminati_pyramid_triangle.png';
-import diamound_shine_expensive_stone from '../../assets/diamound_shine_expensive_stone.png';
-import bulb_pie_chat_light_idea from '../../assets/bulb_pie_chat_light_idea.png';
+import React from "react";
+import Sidebar from "../Entrance-FindTreMos/Sidebar";
+import CreateTremoGeolocation from "./CreateTremoGeolocation";
+import { Link } from "react-router-dom";
+import Eye_illuminati_pyramid_triangle from "../../assets/Eye_illuminati_pyramid_triangle.png";
+import user_like_staff_office_idea from "../../assets/user_like_staff_office_idea.png";
+import marketing_Business_idea_pertinent_Gear from "../../assets/marketing_Business_idea_pertinent_Gear.png";
+
 
 const CreateATreMo = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-      <Sidebar width={300} height={'90vh'}>
-        <div id='CreateTremoSidebarContainer'>
-          <div className='container'>
-            <div id='CreateTremoSidebarItemIcons' className='row container'>
-              <div className='col'>
-                <Link to='/entrancegate'>
-                  <button className='SidebarIconsButtons'>
-                    <img src={Eye_illuminati_pyramid_triangle} alt='Entrance Gate' width='65px' />
-                  </button>
-                </Link>
-              </div>
-              <div className='col'>
-                <Link to='/treasurechest'>
-                  <button className='SidebarIconsButtons'>
-                    <img src={diamound_shine_expensive_stone} alt='Treasure Chest' width='65px' />
-                  </button>
-                </Link>
-              </div>
-              <div className='col'>
-                <Link to='/help'>
-                  <button className='SidebarIconsButtons'>
-                    <img src={bulb_pie_chat_light_idea} alt='help' width='65px' />
-                  </button>
-                </Link>
-              </div>
-            </div>
+    <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+      <Sidebar 
+        width={555}
+        height={"90vh"}
+      >
+        <div id="CreateTremoSidebarContainer">
+          <div id="CreateTremoSidebarItemIcons">
+          <Link to="/entrancegate">
+            <button className="SidebarIconsButtons">
+            <img
+              src={user_like_staff_office_idea}
+              alt="Entrance Gate"
+              width="65px"
+            /></button>
+          </Link>
+          <Link to="/treasurechest">
+            <button className="SidebarIconsButtons">
+            <img
+              src={Eye_illuminati_pyramid_triangle}
+              alt="Treasure Chest"
+              width="65px"
+            /></button>
+          </Link>
+          <Link to="/helpinspirations">
+            <button className="SidebarIconsButtonsRight">
+            <img 
+            src={marketing_Business_idea_pertinent_Gear}
+            alt="HelpInspirations"
+            width="65px" 
+            /></button>
+          </Link>
+          </div>
+          <div id="CreateTremoSidebarItem">
           </div>
           <div id='CreateTremoSidebarItem'></div>
         </div>
--- src/components/Entrance-FindTreMos/EntranceGate.js
+++ src/components/Entrance-FindTreMos/EntranceGate.js
@@ -6,8 +6,10 @@ import FindTremoGeolocation from './FindTremoGeolocation';
 import SidebarTremoCard from './SidebarTremoCard';
 import { distance } from '../../utils/utils';
 import user_like_staff_office_idea from '../../assets/user_like_staff_office_idea.png';
-import diamound_shine_expensive_stone from '../../assets/diamound_shine_expensive_stone.png';
 import bulb_pie_chat_light_idea from '../../assets/bulb_pie_chat_light_idea.png';
+import diamound_shine_expensive_stone from "../../assets/diamound_shine_expensive_stone.png";
+import marketing_Business_idea_pertinent_Gear from "../../assets/marketing_Business_idea_pertinent_Gear.png";
+import Eye_illuminati_pyramid_triangle from "../../assets/Eye_illuminati_pyramid_triangle.png";
 
 const EntranceGate = () => {
   const { userLocation } = useContext(AuthContext);
@@ -27,33 +29,39 @@ const EntranceGate = () => {
   }, [userLocation]);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-      <Sidebar width={'300'} height={'90vh'}>
-        <div id='EntranceGateSidebarContainer'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-4 p-0'>
-                <Link to='/createatremo'>
-                  <button className='SidebarIconsButtons'>
-                    <img src={user_like_staff_office_idea} alt='Create Tremo' width='65px' />
-                  </button>
-                </Link>
-              </div>
-              <div className='col-4 p-0'>
-                <Link to='/treasurechest'>
-                  <button className='SidebarIconsButtons'>
-                    <img src={diamound_shine_expensive_stone} alt='Treasure Chest' width='65px' />
-                  </button>
-                </Link>
-              </div>
-              <div className='col-4 p-0'>
-                <Link to='/help'>
-                  <button className='SidebarIconsButtons'>
-                    <img src={bulb_pie_chat_light_idea} alt='help' width='65px' />
-                  </button>
-                </Link>
-              </div>
-            </div>
+    <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+      <Sidebar
+        width={555}
+        height={"90vh"}
+      >
+        <div id="EntranceGateSidebarContainer">
+          <div className="EntranceGateSidebarItemIcons">
+          <Link to="/createatremo">
+            <button className="SidebarIconsButtons">
+            <img
+              /*src={user_like_staff_office_idea}*/
+              src={diamound_shine_expensive_stone}
+              alt="Create Tremo"
+              width="65px"
+            /></button>
+          </Link>
+          <Link to="/treasurechest">
+            <button className="SidebarIconsButtons">
+            <img
+              /*src={diamound_shine_expensive_stone}*/
+              src={Eye_illuminati_pyramid_triangle}
+              alt="Treasure Chest"
+              width="65px"
+            /></button>
+          </Link>
+          <Link to="/helpinspirations">
+            <button className="SidebarIconsButtonsRight">
+            <img 
+            src={marketing_Business_idea_pertinent_Gear}
+            alt="HelpInspirations"
+            width="65px" 
+            /></button>
+          </Link>
           </div>
           <div id='EntranceGateSidebarItemFindATremoHunt'>
             <SidebarTremoCard tremo={activeHunt} />
