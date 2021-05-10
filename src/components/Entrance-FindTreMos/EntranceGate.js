6064d9ee38c83c5ae4c0d2e9f00d0f2b1aeb1291
Carina Steinberger <carina.steinberger@gmail.com>
2021-05-10 08:57:52 +0200

    cleaning

--- src/components/Entrance-FindTreMos/EntranceGate.js
+++ src/components/Entrance-FindTreMos/EntranceGate.js
@@ -1,39 +1,52 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import FindTremoGeolocation from "./FindTremoGeolocation";
-import CreateBlackTremo from "../../assets/create-black-icon.png";
-import CreateSingleTremoRoute from "../../assets/treasure-map.jpeg";
-import CreateMultipleTremosRoute from "../../assets/multiple-map-journey-map.png";
-import TreasureChest from "../../assets/bright-treasure-chest-icon.jpeg";
 import { Link } from "react-router-dom";
+//import SidebarFindATremoHunt from "./SidebarFindATremoHunt";
 import user_like_staff_office_idea from "../../assets/user_like_staff_office_idea.png";
+import diamound_shine_expensive_stone from "../../assets/diamound_shine_expensive_stone.png";
 import bulb_pie_chat_light_idea from "../../assets/bulb_pie_chat_light_idea.png";
+import BeepleIntoTheEtherTokenArt from "../../assets/BeepleIntoTheEtherTokenArt.jpeg";
 
 const EntranceGate = () => {
   return (
     <div style={{ display: "flex", flexDirection: "row-reverse" }}>
       <Sidebar
-        width={300}
+        width={555}
         height={"90vh"}
-        style={{ backgroundImage: "url(assets/pyramideArtwork.jpeg)" }}
       >
-        <div className="container">
+        <div id="EntranceGateSidebarContainer">
+          <div id="EntranceGateSidebarItemIcons">
           <Link to="/createatremo">
+            <button className="SidebarIconsButtons">
             <img
               src={user_like_staff_office_idea}
               alt="Create Tremo"
-              width="100px"
-            />
+              width="65px"
+            /></button>
             {/* <span className="createATremo">Create a TreMo</span> */}
           </Link>
           <Link to="/treasurechest">
+            <button className="SidebarIconsButtons">
             <img
-              src={bulb_pie_chat_light_idea}
+              src={diamound_shine_expensive_stone}
               alt="TreasureChest"
-              width="100px"
-            />
+              width="65px"
+            /></button>
             {/* <span className="createATremo">Treasure Chest</span> */}
           </Link>
+          <Link to="/help">
+            <button className="SidebarIconsButtons">
+            <img 
+            src={bulb_pie_chat_light_idea}
+            alt="help"
+            width="65px" 
+            /></button>
+          </Link>
+          </div>
+          <div id="EntranceGateSidebarItemFindATremoHunt">
+            {/* <SidebarFindATremoHunt /> */}
+          </div>
         </div>
       </Sidebar>
       <FindTremoGeolocation />
---
+++ src/components/Entrance-FindTreMos/SidebarFindATremoHunt.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SidebarFindeATremoHunt = () => {
+    return
+};
+
+export default SidebarFindeATremoHunt;
